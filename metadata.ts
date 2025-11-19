2eb86182be356762b211779efd62573e2c1427d4
glucrypto <glucrypto@protonmail.com>
2025-11-19 16:56:37 -0500

    fix: added metadata file

---
+++ metadata.ts
@@ -0,0 +1,8 @@
+export const metadata = {
+  metadataSpecVersion: "1.1.0",
+  name: "aave-staking",
+  displayName: "Aave Staking",
+  version: "1.0.0",
+  category: "internal",
+  discovery: { keywords: ["aave", "staking", "opentool"], category: "internal-tools" },
+};
